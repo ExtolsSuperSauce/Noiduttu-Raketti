75fd78fe4420ab0e0697d803258e56f9f04077fe
ExtolsSuperSauce <85796173+ExtolsSuperSauce@users.noreply.github.com>
2025-09-13 05:42:06 -0600

    Fixed a bug

--- extol_space_journey/files/entities/zthulhu/cthulhu.lua
+++ extol_space_journey/files/entities/zthulhu/cthulhu.lua
@@ -6,7 +6,7 @@ function collision_trigger(collider)
 	elseif not GameHasFlagRun("extol_the_leviathan") then
 		EntityKill(collider)
 		local eid = GetUpdatedEntityID()
-		local x,y = GetEntityTransform(eid)
+		local x,y = EntityGetTransform(eid)
 		EntityLoad("data/entities/items/pickup/sun/new_sun.xml",x,y)
 	end
 end
